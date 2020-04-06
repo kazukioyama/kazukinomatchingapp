a34f6d5b4d272423bc7b1d902a8a1604b230028e
KAZUKI OHYAMA <kazukiohyama@KAZUKInoMacBook-Pro.local>
2020-04-06 10:49:06 +0900

    a

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root "top#index"
+
+  resources :users, only: %i(index show)
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :reactions, only: %i(create)
+
+  resources :matching, only: %i(index)
+end
